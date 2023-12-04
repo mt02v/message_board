a35cc919a9452b8eee816736dd73cccbf96b87a9
mt02v <141305346+mt02v@users.noreply.github.com>
2023-12-04 16:34:32 +0000

    [Finish] Boards#destroy

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+get 'boards' => 'boards#index'
+get 'boards/new' => 'boards#new'
+post 'boards' => 'boards#create'
+get 'boards/:id' => 'boards#show'
+get 'boards/:id/edit' => 'boards#edit'
+put 'boards/:id' => 'boards#update'
+delete 'boards/:id' => 'boards#destroy'
+end
